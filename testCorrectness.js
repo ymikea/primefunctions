8ca52b9937416774e61dd6511c94167c5d48f07e
Yekaalo <y.habtemichael@wsu.edu>
2019-12-03 00:57:07 -0800

    "cumulativeSum Test Finished"

--- testCorrectness.js
+++ testCorrectness.js
@@ -15,4 +15,17 @@ describe('Test for Correctness', function () {
       chai.expect(primeGen(20)).deep.equal([2, 3, 5, 7, 11, 13, 17, 19]);
     });
   });
+  /* eslint-env mocha, chai */
+  // eslint-disable-next-line no-redeclare
+  /* global chai, cumulativeSum */
+
+  // cumulativeSum() test cases
+  describe('cumulativeSum', function () {
+    it('cumulativeSum([1, 2, 3, 4]) = [1, 3, 6, 10]', function () {
+      chai.expect(cumulativeSum([1, 2, 3, 4])).deep.equal([1, 3, 6, 10]);
+    });
+    it('cumulativeSum([10, 11, 12, 13, 14]) = [10, 21, 33, 46, 60]', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).deep.equal([10, 21, 33, 46, 60]);
+    });
+  });
 });
